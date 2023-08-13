b3ba4aa61f781370a1f77eb747c3f908f2462ec3
Jean-Marie Pirelli <jean-marie.pirelli@inexto.com>
2023-08-13 18:26:24 +0200

    first file

---
+++ ErrorLoggingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+// code taken from: https://blog.elmah.io/error-logging-middleware-in-aspnetcore/
+
+namespace YourCellarSyncServer.Helper
+{
+    public class ErrorLoggingMiddleware {
+
+        private readonly RequestDelegate _next;
+
+        public ErrorLoggingMiddleware(RequestDelegate next) {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context) {
+            try {
+                await _next(context);
+            } catch (Exception e) {
+                Logger.Exception(e, "Middleware caught the following error.");
+                throw;
+            }
+        }
+    }
+}
